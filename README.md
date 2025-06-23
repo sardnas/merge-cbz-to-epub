37693f97b5893aafdfc7af4a78b73d5dc26bee1d
sardnas <sandrareinecke19@gmail.com>
2025-06-23 21:39:05 +0200

    add script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # merge-cbz-to-epub
+
+How to use:
+1. Go to script, ex: /Code/merge-cbz-to-epub
+2. Write: node script.js "/home/[location of .cbz files]"
+3. The output.epub file will be generated in the same folder
---
+++ script.js
@@ -0,0 +1,52 @@
+const fs = require('fs-extra');
+const path = require('path');
+const AdmZip = require('adm-zip');
+const tmp = require('tmp');
+const Epub = require('epub-gen');
+
+const inputFolder = process.argv[2];
+if (!inputFolder) {
+  console.error("Usage: node cbz-to-epub.js <folder_with_cbz_files>");
+  process.exit(1);
+}
+
+(async () => {
+  const cbzFiles = (await fs.readdir(inputFolder))
+    .filter(file => file.toLowerCase().endsWith('.cbz'))
+    .sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
+
+  const tmpDir = tmp.dirSync({ unsafeCleanup: true });
+  const chapters = [];
+
+  for (const cbzFile of cbzFiles) {
+    const fullPath = path.join(inputFolder, cbzFile);
+    const zip = new AdmZip(fullPath);
+    const chapterDir = path.join(tmpDir.name, path.basename(cbzFile, '.cbz'));
+
+    zip.extractAllTo(chapterDir, true);
+    const images = (await fs.readdir(chapterDir))
+      .filter(file => /\.(jpe?g|png|gif)$/i.test(file))
+      .sort();
+
+    const htmlContent = images.map(img =>
+    `<img src="${path.join(chapterDir, img)}" style="width:100%; display: block; margin-bottom: 1em;"/>`
+    ).join('');
+      
+    chapters.push({
+      data: htmlContent,
+    });
+  }
+
+  const outputEpub = path.join(process.cwd(), 'output.epub');
+  const option = {
+    title: "Title",
+    author: "Author",
+    content: chapters,
+  };
+
+  console.log("Generating EPUB...");
+  await new Epub(option, outputEpub).promise;
+  console.log("EPUB created at:", outputEpub);
+
+  tmpDir.removeCallback();
+})();
